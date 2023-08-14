c1d829ae57a2fcd88de1d3f5353096fe7ff92183
Ayu Ningtias <ayulin9900@gmail.com>
2023-08-14 23:20:00 +0700

    ubah profil dan foto

--- resources/views/frontend/users/profile.blade.php
+++ resources/views/frontend/users/profile.blade.php
@@ -1,14 +1,10 @@
 @extends('layouts.frontend')
 @section('title', 'Profile Saya')
 @section('content')
-	<div class="breadcrumb-area pt-205 breadcrumb-padding pb-210" style="background-image: url({{ asset('frontend/assets/img/bg/ayu.jpg') }})">
+	<div class="breadcrumb-area pt-205 breadcrumb-padding pb-210" style="background-image: url({{ asset('frontend/assets/img/bg/ayuN.jpg') }})">
 		<div class="container-fluid">
 			<div class="breadcrumb-content text-center">
-				<h2>Daftar</h2>
-				<ul>
-					<li><a href="#">home</a></li>
-					<li>Daftar</li>
-				</ul>
+				<h2>AKUN SAYA</h2>
 			</div>
 		</div>
 	</div>
@@ -21,12 +17,12 @@
 			@endif
 			<div class="row">
 				<div class="col-lg-3">
-                    <h3 class="sidebar-title">User Menu</h3>
+                    <h3 class="sidebar-title">Menu Penggna</h3>
                     <div class="sidebar-categories">
                         <ul>
-                            <li><a href="{{ url('profile') }}">Profile</a></li>
-                            <li><a href="{{ url('orders') }}">Orders</a></li>
-                            <li><a href="{{ url('favorites') }}">Favorites</a></li>
+                            <li><a href="{{ url('profile') }}">Profil</a></li>
+                            <li><a href="{{ url('orders') }}">Pesanan</a></li>
+                            <li><a href="{{ url('favorites') }}">Favorit</a></li>
                         </ul>
                     </div>
 				</div>
@@ -48,14 +44,14 @@
 									</div>
 									<div class="form-group row">
 										<div class="col-md-6">
-                                            <label for="first_name">First Name</label>
+                                            <label for="first_name">Nama Depan</label>
                                             <input type="text" name="first_name" value="{{ auth()->user()->first_name }}">
                                             @error('first_name')
 												<div class="alert alert-danger">{{ $message }}</div>
 											@enderror
 										</div>
 										<div class="col-md-6">
-                                            <label for="last_name">Last Name</label>
+                                            <label for="last_name">Nama Belakang</label>
                                             <input type="text" name="last_name" value="{{ auth()->user()->last_name }}">
                                             @error('last_name')
 												<div class="alert alert-danger">{{ $message }}</div>
@@ -65,7 +61,7 @@
 
 									<div class="form-group row">
 										<div class="col-md-12">
-                                            <label for="address1">Address1</label>
+                                            <label for="address1">Alamat 1</label>
                                             <textarea name="address1" rows="5">{{ auth()->user()->address1 }}</textarea>
 											@error('address1')
 												<div class="alert alert-danger">{{ $message }}</div>
@@ -75,7 +71,7 @@
 
 									<div class="form-group row">
 										<div class="col-md-12">
-                                            <label for="address2">Address2</label>
+                                            <label for="address2">Alamat 2</label>
                                             <textarea name="address2" rows="5">{{ auth()->user()->address2 }}</textarea>
 											@error('address2')
 												<div class="alert alert-danger">{{ $message }}</div>
@@ -85,7 +81,7 @@
 
 									<div class="form-group row">
 										<div class="col-md-6">
-                                            <label>Province<span class="required">*</span></label>
+                                            <label>Provinsi<span class="required">*</span></label>
                                             <select name="province_id" id="province-id" value="{{ auth()->user()->province_id }}">
                                                     <option value="">- Please Select -</option>
                                                 @foreach($provinces as $province => $pro)
@@ -97,7 +93,7 @@
 											@enderror
 										</div>
 										<div class="col-md-6">
-                                            <label>City<span class="required">*</span></label>
+                                            <label>KOta<span class="required">*</span></label>
                                             <select name="city_id" id="city-id"  value="{{ auth()->user()->city_id }}" >
                                                 @foreach($cities as $city => $ty)
                                                     <option {{ auth()->user()->city_id == $city ? 'selected' : null }} value="{{ $city }}">{{ $ty }}</option>
@@ -111,14 +107,14 @@
 
 									<div class="form-group row">
 										<div class="col-md-6">
-                                        <label>Postcode / Zip <span class="required">*</span></label>						
+                                        <label>Kode Pos <span class="required">*</span></label>						
 									    <input type="number" name="postcode" placeholder="PostalCode..." value="{{ auth()->user()->postcode }}">
 											@error('postcode')
 												<div class="alert alert-danger">{{ $message }}</div>
 											@enderror
 										</div>
 										<div class="col-md-6">
-                                            <label>Phone  <span class="required">*</span></label>		
+                                            <label>Nomor Handphone  <span class="required">*</span></label>		
 									        <input type="number" name="phone" placeholder="Phone..." value="{{ auth()->user()->phone }}">
 											@error('phone')
 												<div class="alert alert-danger">{{ $message }}</div>
@@ -128,7 +124,7 @@
 
 									<div class="form-group row">
 										<div class="col-md-12">
-                                            <label>Email Address </label>
+                                            <label>Alamat E-mail</label>
 									        <input type="text" name="email" placeholder="Email..." value="{{ auth()->user()->email}}">		
 											@error('email')
 												<div class="alert alert-danger">{{ $message }}</div>
@@ -136,7 +132,7 @@
 										</div>
 									</div>
 									<div class="button-box">
-										<button type="submit" class="default-btn floatright">Update Profile</button>
+										<button type="submit" class="default-btn floatright">Perbarui Profil</button>
 									</div>
                                 </form>
 							</div>
